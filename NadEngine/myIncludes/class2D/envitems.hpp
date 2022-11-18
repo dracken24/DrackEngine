7e6c90d3d914e4274465260968d77911a8ea88d0
dracken24 <dracken24@gmail.com>
2022-11-17 19:02:09 -0500

    first

---
+++ NadEngine/myIncludes/class2D/envitems.hpp
@@ -0,0 +1,76 @@
+
+#ifndef ENVITEM_HPP
+#define ENVITEM_HPP
+
+#include "../../vendor/raylib/src/raylib.h"
+#include "../../../myIncludes/class2D/envitems.hpp"
+#include <stdlib.h>
+#include <iostream>
+
+namespace obj
+{
+	typedef struct VarCharEnvi
+	{
+		char *enviPosX;
+		char *enviPosY;
+		char *enviWidth;
+		char *enviHeight;
+		int nbr = 4;
+	} VarCharEnvi;
+
+	typedef struct EnvItem
+	{
+		Texture2D texture;
+		Color color;
+		Rectangle rect;
+		VarCharEnvi _varCharEnvi;
+		Color pixColor;
+
+		int blocking;
+		int nbr;
+
+		std::string name;
+	} EnvItem;
+
+	class EnvItems
+	{
+	public:
+		EnvItems(void);
+		EnvItems(EnvItems const &src);
+		~EnvItems(void);
+
+		// void        ftInitEnvitem(Vector2 pos, Vector2 size,
+		// 				int blocking, Color color, Texture2D texture, int nbr);
+		VarCharEnvi *ftReturnVarsCharEnvi(void);
+		// void		ftNewEnvItem(int nbr);
+		// Rectangle	ftReturnRectangle(int nbr) const;
+		// Vector2		ftReturnEnviPos(int nbr) const;
+		// Vector2		ftReturnEnviSize(int nbr) const;
+		// Color		ftReturnEnviColor(int nbr) const;
+		// Texture2D	ftReturnEnviTexture(int nbr) const;
+		// EnvItem		*ftReturnEnvitemPtr(int nbr);
+		int ftReturnEnviAllNbr(void) const;
+		// EnvItems	*ftReturnCopyEnvItems(void);
+
+		// One item
+		EnvItems *ftInitOneEnvitem(Game *game, Vector2 pos, Vector2 size, int blocking,
+								   Color color, Texture2D texture, int nbr, std::string name);
+		Rectangle ftReturnOneRectangle(void) const;
+		Vector2 ftReturnOneEnviPos(void) const;
+		Vector2 ftReturnOneEnviSize(void) const;
+		Color ftReturnOneEnviColor(void) const;
+		Texture2D ftReturnOneEnviTexture(void) const;
+		EnvItem *ftReturnOneEnvitemPtr(void);
+		EnvItems *ftReturnOneEnvitems(void);
+
+		void ftInitVarChar(void);
+		void ftDeleteVarChar(void);
+		void ftDestroyImgs(void);
+
+	private:
+		EnvItem _item;
+		VarCharEnvi	_varCharEnvi;
+	};
+};
+
+#endif
---
+++ NadEngine/src/2D/class2D/envitems.cpp
@@ -0,0 +1,164 @@
+#include "../../../myIncludes/game.hpp"
+
+using namespace obj;
+
+EnvItems::EnvItems(void)
+{
+	return;
+}
+
+EnvItems::EnvItems(EnvItems const &src)
+{
+	*this = src;
+	return;
+}
+
+EnvItems::~EnvItems(void)
+{
+	return;
+}
+
+// void	EnvItems::ftNewEnvItem(int nbr)
+// {
+
+// 	this->_envItems = new EnvItem[nbr];
+// 	this->_allNbr = nbr;
+// }
+
+// EnvItems	*EnvItems::ftReturnCopyEnvItems(void)
+// {
+
+// 	EnvItems *ret = new EnvItems;
+
+// 	ret = this;
+// 	return (ret);
+// }
+
+// void 	EnvItems::ftInitEnvitem(Vector2 pos, Vector2 size,
+// 			int blocking, Color color, Texture texture, int nbr)
+// {
+// 	this->_envItems[nbr].rect.x = pos.x;
+// 	this->_envItems[nbr].rect.y = pos.y;
+// 	this->_envItems[nbr].rect.width = size.x;
+// 	this->_envItems[nbr].rect.height = size.y;
+// 	this->_envItems[nbr].color = color;
+// 	this->_envItems[nbr].texture = texture;
+// 	this->_envItems[nbr].blocking = blocking;
+// 	this->_envItems[nbr].nbr = nbr;
+// 	this->_envItems[nbr]._varCharEnvi.enviPosX = (char *)calloc(sizeof(char), 9);
+// 	this->_envItems[nbr]._varCharEnvi.enviPosY = (char *)calloc(sizeof(char), 9);
+// 	this->_envItems[nbr]._varCharEnvi.enviWidth = (char *)calloc(sizeof(char), 9);
+// 	this->_envItems[nbr]._varCharEnvi.enviHeight = (char *)calloc(sizeof(char), 9);
+// }
+EnvItems *EnvItems::ftInitOneEnvitem(Game *game, Vector2 pos, Vector2 size, int blocking,
+									 Color color, Texture texture, int nbr, std::string name)
+{
+	this->_item.rect.x = pos.x;
+	this->_item.rect.y = pos.y;
+	this->_item.rect.width = size.x;
+	this->_item.rect.height = size.y;
+	this->_item.color = color;
+	this->_item.texture = texture;
+	this->_item.blocking = blocking;
+	this->_item.nbr = nbr;
+	this->_item.name = name;
+	game->nbrEnvItems += 1;
+
+	return (this);
+}
+
+void EnvItems::ftInitVarChar(void)
+{
+	this->_varCharEnvi.enviPosX = (char *)calloc(sizeof(char), 9);
+	this->_varCharEnvi.enviPosY = (char *)calloc(sizeof(char), 9);
+	this->_varCharEnvi.enviWidth = (char *)calloc(sizeof(char), 9);
+	this->_varCharEnvi.enviHeight = (char *)calloc(sizeof(char), 9);
+}
+
+void EnvItems::ftDeleteVarChar(void)
+{
+	free(this->_varCharEnvi.enviPosX);
+	free(this->_varCharEnvi.enviPosY);
+	free(this->_varCharEnvi.enviWidth);
+	free(this->_varCharEnvi.enviHeight);
+}
+
+EnvItems *EnvItems::ftReturnOneEnvitems(void)
+{
+	EnvItems *ret = new EnvItems;
+
+	ret = this;
+	return (ret);
+}
+
+// EnvItem		*EnvItems::ftReturnEnvitemPtr(int nbr)
+// {
+// 	return (&this->_envItems[nbr]);
+// }
+
+// Rectangle	EnvItems::ftReturnRectangle(int nbr) const
+// {
+// 	return (this->_envItems[nbr].rect);
+// }
+
+// Vector2		EnvItems::ftReturnEnviPos(int nbr) const
+// {
+// 	return ((Vector2){this->_envItems[nbr].rect.x, this->_envItems[nbr].rect.y});
+// }
+
+// Vector2		EnvItems::ftReturnEnviSize(int nbr) const
+// {
+// 	return ((Vector2){this->_envItems[nbr].rect.width, this->_envItems[nbr].rect.height});
+// }
+
+// Color		EnvItems::ftReturnEnviColor(int nbr) const
+// {
+// 	return (this->_envItems[nbr].color);
+// }
+
+// Texture2D	EnvItems::ftReturnEnviTexture(int nbr) const
+// {
+// 	return (this->_envItems[nbr].texture);
+// }
+
+// int			EnvItems::ftReturnEnviAllNbr(void) const
+// {
+// 	return (this->_allNbr);
+// }
+
+//=-= One item =-=//
+
+EnvItem *EnvItems::ftReturnOneEnvitemPtr(void)
+{
+	return (&this->_item);
+}
+
+Rectangle EnvItems::ftReturnOneRectangle(void) const
+{
+	return (this->_item.rect);
+}
+
+Vector2 EnvItems::ftReturnOneEnviPos(void) const
+{
+	return ((Vector2){this->_item.rect.x, _item.rect.y});
+}
+
+Vector2 EnvItems::ftReturnOneEnviSize(void) const
+{
+	return ((Vector2){this->_item.rect.width, _item.rect.height});
+}
+
+Color EnvItems::ftReturnOneEnviColor(void) const
+{
+	return (this->_item.color);
+}
+
+Texture2D EnvItems::ftReturnOneEnviTexture(void) const
+{
+	return (this->_item.texture);
+}
+
+void EnvItems::ftDestroyImgs(void)
+{
+	UnloadTexture(this->_item.texture);
+}
