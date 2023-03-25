91d200fd9eb43834a41f31a8fd0ff114698ad800
dracken24 <dracken24@gmail.com>
2023-03-25 13:25:13 -0400

    Start rebuild

---
+++ RebuiltEngine/Code/srcs/main.cpp
@@ -0,0 +1,41 @@
+/*****************************************************************************/
+/*\|/~---~---~---~---~---~---~---~---~---~---~---~---~---~---~---~---~---~\|/*/
+/* |             ---------------------------------------------             | */
+/* |             *--*  PROJET: DrackenLib PAR: Dracken24  *--*             | */
+/* |             ---------------------------------------------             | */
+/* |             *--*  DATE:		 24-03-2023  		  *--*             | */
+/* |             ---------------------------------------------             | */
+/* |             *--*  FILE: 	      main.hpp            *--*             | */
+/* |             ---------------------------------------------             | */
+/*/|\~---~---~---~---~---~---~---~---~---~---~---~---~---~---~---~---~---~/|\*/
+/*****************************************************************************/
+
+#include "../includes/core.hpp"
+
+int main(void)
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "DrackEngine", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+}
---
+++ RebuiltEngine/bin/srcs/main.d
@@ -0,0 +1,4 @@
+bin/srcs/main.o: Code/srcs/main.cpp Code/srcs/../includes/core.hpp \
+ Code/srcs/../includes/./color.hpp
+Code/srcs/../includes/core.hpp:
+Code/srcs/../includes/./color.hpp:
